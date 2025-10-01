0f231287e658b00b6ac448c5e6cc1b4d4312721c
Emeopal <2593652329@qq.com>
2025-10-01 20:24:36 +0800

    commit

---
+++ Shot/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
+        }
+    }
+}
